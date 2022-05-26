11218abb996a409074b4aae16d7f9097073a7394
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2022-05-26 13:54:42 +0200

    Using a re-connecting websocket wrapper

--- main.js
+++ main.js
@@ -1,47 +1,82 @@
+class Connection
+{
+    constructor(url)
+    {
+        this.url = url;
+        this._openWS();
+    }
+
+    destroy()
+    {
+        if (!this.ws)
+            return;
+
+        this.ws.onclose = null;
+        this.ws.onmessage = null;
+        this.ws.onopen = null;
+        this.ws.close();
+        this.ws = null;
+        console.log("Websocket command connection destroyed");
+    }
+
+    onConnectionStateChanged(connected) { } // Override this!
+
+    _openWS()
+    {
+        this.ws = new WebSocket(this.url);
+        this.ws.onclose = () => { this._onWSClose(); this.onConnectionStateChanged(false); }
+        this.ws.onopen = () => { this.onConnectionStateChanged(true); }
+        this.ws.onmessage = (msg) => { this.onMessage(msg); }
+    }
+
+    _onWSClose()
+    {
+        this.ws.onclose = null;
+        this.ws.onmessage = null;
+        this.ws.onopen = null;
+        this.ws = null;
+        setTimeout(() => { 
+            console.log("Trying to re-open websocket");
+            this._openWS()
+        }, 1000); // Wait a bit and try again
+    }
+
+    onMessage(msg) // Override if needed!
+    {
+        console.log("Received message on WebSocket:");
+        console.log(msg);
+    }
+
+    sendCommand(obj)
+    {
+        if (this.ws)
+            this.ws.send(JSON.stringify(obj));
+    }
+}
+
+let conn = null;
+
+function onConnectionStateChanged(connected)
+{
+    let tbl = document.getElementById("maintable");
+    let d = document.getElementById("connecting");
+    if (connected)
+    {
+        tbl.style.display = "";
+        d.style.display = "none";
+    }
+    else
+    {
+        tbl.style.display = "none";
+        d.style.display = "";
+    }
+}
+
 function log(s)
 {
     var e = document.getElementById("log");
     var txt = e.innerText;
     e.innerText = txt + "\n" + s;
-}
-
-var ws = null;
-var connected = false;
-
-function onConnected()
-{
-    connected = true;
-
-    var tbl = document.getElementById("maintable");
-    tbl.style.display = "";
-
-    var d = document.getElementById("connecting");
-    d.style.display = "none";
-
-    var el = document.getElementById("touchdiv");
-    el.addEventListener("touchstart", handleStart, false);
-    el.addEventListener("touchend", handleEnd, false);
-    el.addEventListener("touchcancel", handleCancel, false);
-    el.addEventListener("touchmove", handleMove, false);
-
-    var b = document.getElementById("sendbutton");
-    b.onclick = onSendText;
-}
-
-function onClose()
-{
-    if (!connected)
-    {
-        var d = document.getElementById("connecting");
-        d.innerHTML += "<br>Connection failed!";
-        return;
-    }
-
-    var el = document.getElementById("touchdiv");
-    el.style.background = "red";
-    el.style.color = "white";
-
-    el.innerHTML = "<p>CONNECTION LOST!</p><p><button onclick='location.reload(false)'>Reload</button></p>";
 }
 
 var threshold = 10;
@@ -85,7 +120,7 @@ function handleEnd(e)
             var dt = Date.now() - at.time;
             var diff2 = at.relx*at.relx+at.rely*at.rely;
             if (dt < 500 && diff2 < 16)
-                ws.send(JSON.stringify({ "type": "click", "button": 1 }));
+                conn.sendCommand({ "type": "click", "button": 1 });
         }
     }
 
@@ -113,7 +148,7 @@ function processScroll(dyLeft)
         if (scrollSum < 1)
             break;
 
-        ws.send(JSON.stringify({ "type": "scroll", "num": 1 }));
+        conn.sendCommand({ "type": "scroll", "num": 1 });
         scrollSum -= 1;
     }
 
@@ -122,7 +157,7 @@ function processScroll(dyLeft)
         if (scrollSum > -1)
             break;
 
-        ws.send(JSON.stringify({ "type": "scroll", "num": -1 }));
+        conn.sendCommand({ "type": "scroll", "num": -1 });
         scrollSum += 1;
     }
 }
@@ -157,9 +192,9 @@ function handleMove(e)
             {
                 var q = dxLeft*dxLeft+dyLeft*dyLeft;
                 if (q <= threshold*threshold)
-                    ws.send(JSON.stringify({ "type": "moverel", "dx": dxLeft, "dy": dyLeft }));
+                    conn.sendCommand({ "type": "moverel", "dx": dxLeft, "dy": dyLeft });
                 else
-                    ws.send(JSON.stringify({ "type": "moverel", "dx": dxLeft*accel, "dy": dyLeft*accel }));
+                    conn.sendCommand({ "type": "moverel", "dx": dxLeft*accel, "dy": dyLeft*accel });
             }
         }
     }
@@ -193,13 +228,13 @@ function onSendText()
                 {
                     log("sending " + code + " " + modifier);
                     if (modifier !== null)
-                        ws.send(JSON.stringify({ "type": "key", "keycode": modifier, "pressed": true }));
+                        conn.sendCommand({ "type": "key", "keycode": modifier, "pressed": true });
                     
-                    ws.send(JSON.stringify({ "type": "key", "keycode": code, "pressed": true }));
-                    ws.send(JSON.stringify({ "type": "key", "keycode": code, "pressed": false }));
+                    conn.sendCommand({ "type": "key", "keycode": code, "pressed": true });
+                    conn.sendCommand({ "type": "key", "keycode": code, "pressed": false });
                     
                     if (modifier !== null)
-                        ws.send(JSON.stringify({ "type": "key", "keycode": modifier, "pressed": false }));
+                        conn.sendCommand({ "type": "key", "keycode": modifier, "pressed": false });
                     break;
                 }
             }
@@ -213,19 +248,27 @@ function onKeyButton(keyName)
     if (!code)
         return;
 
-    ws.send(JSON.stringify({ "type": "key", "keycode": code, "pressed": true }));
-    ws.send(JSON.stringify({ "type": "key", "keycode": code, "pressed": false }));
+    conn.sendCommand({ "type": "key", "keycode": code, "pressed": true });
+    conn.sendCommand({ "type": "key", "keycode": code, "pressed": false });
 }
 
 function main()
 {
-    log("V16");
+    log("V17");
     try
     {
-        var wsUrl = "ws://" + location.hostname + ":8081";
-        ws = new WebSocket(wsUrl);
-        ws.onopen = onConnected;
-        ws.onclose = onClose;
+        let wsUrl = "ws://" + location.hostname + ":8081";
+        conn = new Connection(wsUrl);
+        conn.onConnectionStateChanged = onConnectionStateChanged;
+
+        let el = document.getElementById("touchdiv");
+        el.addEventListener("touchstart", handleStart, false);
+        el.addEventListener("touchend", handleEnd, false);
+        el.addEventListener("touchcancel", handleCancel, false);
+        el.addEventListener("touchmove", handleMove, false);
+
+        let b = document.getElementById("sendbutton");
+        b.onclick = onSendText;
     } 
     catch(e)
     {
